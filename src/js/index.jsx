751452a8c8546beb53bb8f3891222c6cf66e53bc
Taimoor Khan <taimoor.m.k@live.com>
2018-05-12 00:03:52 +0500

    application barebones added

---
+++ src/js/index.jsx
@@ -0,0 +1,34 @@
+// ########## Import Dependencies Here ##########
+import ReactDOM from 'react-dom';
+import React from 'react';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import reduxLogger from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
+
+// ########## Import Containers Here ##########
+
+// ########## Import Components Here ##########
+import App from './components/App';
+import rootReducer from './reducers';
+import rootSaga from './sagas';
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+  rootReducer,
+  window.devToolsExtension && window.devToolsExtension(),
+  applyMiddleware(
+    sagaMiddleware, 
+    reduxLogger
+  )
+);
+
+sagaMiddleware.run(rootSaga);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#app')
+);
